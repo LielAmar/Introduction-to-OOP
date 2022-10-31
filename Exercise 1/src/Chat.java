42c580218cea2161e16db5deaf0e469ba552d0d0
Liel Amar <lielamar@oversight.co.il>
2022-10-31 09:11:06 +0200

    ex 1

---
+++ Exercise 1/src/Chat.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Chat {
+
+    public static void main(String[] args) {
+        ChatterBot[] bots = {
+                new ChatterBot(
+                    "Ash",
+                    new String[] {
+                            "Uhm, " + ChatterBot.REQUESTED_PHRASE_PLACEHOLDER + "? Alright: " +
+                                ChatterBot.REQUESTED_PHRASE_PLACEHOLDER,
+                            ChatterBot.REQUESTED_PHRASE_PLACEHOLDER + " it is!",
+                            "I like it! " + ChatterBot.REQUESTED_PHRASE_PLACEHOLDER
+                    },
+                    new String[] {
+                            "What do you even want?",
+                            "I don't want to say " + ChatterBot.ILLEGAL_REQUEST_PLACEHOLDER + "!",
+                            "I have no idea what " + ChatterBot.ILLEGAL_REQUEST_PLACEHOLDER + " means...",
+                            ChatterBot.REQUEST_PREFIX + ChatterBot.ILLEGAL_REQUEST_PLACEHOLDER
+                    }
+                ),
+                new ChatterBot(
+                    "Pikachu",
+                    new String[] {
+                            "Hey! " + ChatterBot.REQUESTED_PHRASE_PLACEHOLDER + "? Okay :)",
+                            "Are you sure? Fine... " + ChatterBot.REQUESTED_PHRASE_PLACEHOLDER
+                    },
+                    new String[] {
+                            "what is " + ChatterBot.ILLEGAL_REQUEST_PLACEHOLDER + "?",
+                            "I can't say " + ChatterBot.ILLEGAL_REQUEST_PLACEHOLDER + "!",
+                            ChatterBot.REQUEST_PREFIX + ChatterBot.ILLEGAL_REQUEST_PLACEHOLDER
+                    }
+                )
+        };
+
+        Scanner scanner = new Scanner(System.in);
+        String statement = scanner.nextLine();
+
+        while(true) {
+            for(ChatterBot bot : bots) {
+                statement = bot.replyTo(statement);
+                System.out.print(bot.getName() + ": " + statement);
+                scanner.nextLine();
+            }
+        }
+    }
+}
---
+++ Exercise 1/src/ChatterBot.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+/**
+ * Base file for the ChatterBot exercise.
+ * The bot's replyTo method receives a statement.
+ * If it starts with the constant REQUEST_PREFIX, the bot returns
+ * whatever is after this prefix. Otherwise, it returns one of
+ * a few possible replies as supplied to it via its constructor.
+ * In this case, it may also include the statement after
+ * the selected reply (coin toss).
+ * @author Dan Nirel
+ */
+class ChatterBot {
+
+	public static final String REQUESTED_PHRASE_PLACEHOLDER = "<phrase>";
+	public static final String ILLEGAL_REQUEST_PLACEHOLDER = "<request>";
+	public static final String REQUEST_PREFIX = "say ";
+
+	private final String name;
+	private final Random rand = new Random();
+	private final String[] repliesToLegalRequest;
+	private final String[] repliesToIllegalRequest;
+
+	public ChatterBot(String name, String[] repliesToLegalRequest, String[] repliesToIllegalRequest) {
+		this.name = name;
+		this.repliesToLegalRequest = new String[repliesToLegalRequest.length];
+		this.repliesToIllegalRequest = new String[repliesToIllegalRequest.length];
+
+		System.arraycopy(repliesToLegalRequest, 0, this.repliesToLegalRequest, 0, repliesToLegalRequest.length);
+		System.arraycopy(repliesToIllegalRequest, 0, this.repliesToIllegalRequest, 0, repliesToIllegalRequest.length);
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public String replyTo(String statement) {
+		if(statement.startsWith(REQUEST_PREFIX)) {
+			return this.replacePlaceholderInARandomPattern(
+					statement.replaceFirst(REQUEST_PREFIX, ""), // we don’t repeat the request prefix, so delete it from the reply
+					REQUESTED_PHRASE_PLACEHOLDER,
+					this.repliesToLegalRequest
+			);
+		}
+
+		return this.replacePlaceholderInARandomPattern(
+				statement,
+				ILLEGAL_REQUEST_PLACEHOLDER,
+				this.repliesToIllegalRequest
+		);
+	}
+
+	private String replacePlaceholderInARandomPattern(String statement, String placeholder, String[] replies) {
+		int randomIndex = this.rand.nextInt(replies.length);
+
+		String reply = replies[randomIndex];
+
+		return reply.replaceAll(placeholder, statement);
+	}
+}
